f8b3124662203e84b1195bafa2aafbab9dbd9386
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2021-09-10 11:04:54 +0200

    Adding Test Object with Test Script

---
+++ VR_Game1/Assets/Scripts/ScriptTesting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HELLO WORLD THIS IS A TEST!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
