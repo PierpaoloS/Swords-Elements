9ab5f87f5a820420c50d5b9a657d3af24abaa947
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2021-10-10 10:53:25 +0200

    Adding tornado super Power and Tornado Spawn script

--- VR_Game1/Assets/Scripts/TornadoSpawner.cs
+++ VR_Game1/Assets/Scripts/TornadoSpawner.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
-
+using UnityEngine.XR;
 public class TornadoSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private InputDevice targetDevice;
+    public GameObject tornado;
+    public GameObject player;
+    public float spawnDistance;
+    private bool isTornadoBuilt = false;
+
+    
     void Start()
     {
-        
-    }
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
 
-    // Update is called once per frame
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    
+    }
     void Update()
     {
-        
+        //Bottone X
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+        if (primaryButtonValue && isTornadoBuilt == false)
+        {
+            MagicTornado();
+        }
+    }
+    
+    private void MagicTornado()
+    {
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerDirection = player.transform.forward;
+        Quaternion playerRotation = player.transform.rotation;
+        Vector3 spawnPos = playerPos + playerDirection * spawnDistance;
+        // Debug.Log("Grilletto Premuto!!!");
+        Instantiate(tornado, spawnPos, playerRotation);
+        isTornadoBuilt = true;
+        Invoke("ResetTornadoCounter", 5.0f);
+    }
+
+    private void ResetTornadoCounter()
+    {
+        //Destroy(tornado);
+        isTornadoBuilt = false;
     }
 }
