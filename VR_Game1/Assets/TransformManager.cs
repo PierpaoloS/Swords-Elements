fc35657a2de09d7306654005441907f57bda1745
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2021-10-23 15:41:59 +0200

    Spawn Wall and Tornado Fixed.(Serrano e Ziza)
    CONTRATTEMPO: poichè a Ziza gli si è fuso il computer, abbiamo lavorato assieme su questa parte tramite meeting su discord.

---
+++ VR_Game1/Assets/TransformManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.WebCam;
+
+public class TransformManager : MonoBehaviour
+{
+    private float rotX;
+    private float rotY;
+    private float rotZ;
+    
+    public GameObject vrRig;
+
+    public GameObject cam;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotX = vrRig.transform.eulerAngles.x;
+        rotY = cam.transform.eulerAngles.y;
+        rotZ = vrRig.transform.eulerAngles.z;
+
+        this.transform.rotation = Quaternion.Euler(rotX, rotY, rotZ);
+    }
+}
