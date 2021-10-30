848b334db60627816992c382e1fb2b9acaf59c94
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2021-10-30 14:10:08 +0200

    AddingMoreStuff

--- VR_Game1/Assets/TransformManager.cs
+++ VR_Game1/Assets/TransformManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows.WebCam;
+//using UnityEngine.Windows.WebCam;
 
 public class TransformManager : MonoBehaviour
 {
