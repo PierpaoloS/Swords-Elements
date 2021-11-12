c3a7f72b8cc812b619e358b0d56282de2ab99457
PierpaoloS <serrano.pierpaolo96@yahoo.it>
2021-11-12 17:38:26 +0100

    fixingParticlesEffectOnRocks (Serrano & Ziza)

---
+++ VR_Game1/Assets/Scripts/ExplosionParticles.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionParticles : MonoBehaviour
+{
+   public ParticleSystem explosion;
+   private void OnCollisionEnter(Collision other)
+   {
+      ContactPoint contact = other.contacts[0];
+      Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+      Vector3 pos = contact.point;
+      ParticleSystem particleExplosion = Instantiate(explosion, pos, rot);
+      Destroy(gameObject);
+   }
+}
